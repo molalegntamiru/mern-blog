f2d1f4766d7e5dac56a830f4b05030c75291d70f
molalegnTmiru <molalegn.tamiru@gmail.com>
2024-07-19 03:17:19 -0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log(`database connected`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
